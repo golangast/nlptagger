1ecc768e8a92a024ee69e639201c53afc8b1a872
zendrulat <zendrulat@gmail.com>
2024-10-08 11:47:21 +0000

    updated printing

--- main.go
+++ main.go
@@ -1,19 +1,48 @@
 package main
 
-//"fmt"
-//"github.com/golangast/nlptagger/tagger"
+// "fmt"
+
+// "github.com/golangast/nlptagger/tagger"
 
 func main() {
 
 	// t := tagger.Tagging("Create a database named Inventory with the data structure Product containing 2 string fields and 1 integer field.")
-	// for i := range t.Tokens {
-	// 	fmt.Printf("Tokens: %s\t\t\t Ner: %s\t\t Pos: %s\t\t PhraseTag: %s\t\t  \n", t.Tokens[i], t.NerTag[i], t.PosTag[i], t.PhraseTag[i])
-	// }
-	//**VB** create a **NN** webserver **VBN** named **NNP Doggy** with the **NN** handler **NNP Kitty** that **VBZ** has the **NN** data structure **NNP Moose** with **CD** 4 **NN** string fields
-	//t := modelname.Trainer("create a webserver named doggy with the handler kitty that has the data structure moose with 4 string fields")
-	// for i := range t.Tokens {
-	// 	if t.NerTag[i] != "" { // Check if the Ner tag has a value
-	// 		fmt.Printf("Epoch %d, Cost: %f, isName: %t, Tokens: %s, Ner: %s,Pos: %s, PhraseTag: %s,  features %v\n", i, t.Cost, t.IsName, t.Tokens[i], t.NerTag[i], t.PosTag[i], t.PhraseTag[i], t.Features[i])
-	// 	}
-	// }
+	// combined := append(t.Tokens, t.PosTag...)
+	// table(combined, 2)
+	// fmt.Println("Pos is done")
+	// fmt.Println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+	// combined2 := append(t.Tokens, t.NerTag...)
+	// table(combined2, 2)
+	// fmt.Println("Ner is done")
+	// fmt.Println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
+	// combined3 := append(combined, t.PhraseTag...)
+	// table(combined3, 2)
+
 }
+
+// func table(input []string, cols int) {
+// 	maxWidth := 0
+// 	for _, s := range input {
+// 		if len(s) > maxWidth {
+// 			maxWidth = len(s)
+// 		}
+// 	}
+// 	format := fmt.Sprintf("%%d.%%-%ds%%s", maxWidth)
+
+// 	rows := (len(input) + cols - 1) / cols
+// 	for row := 0; row < rows; row++ {
+// 		for col := 0; col < cols; col++ {
+// 			i := col*rows + row
+// 			if i >= len(input) {
+// 				break // This means the last column is not "full"
+// 			}
+// 			padding := ""
+// 			if i < 9 {
+// 				padding = " "
+// 			}
+// 			fmt.Printf(format, i+1, input[i], padding)
+// 		}
+// 		fmt.Println()
+// 	}
+// }
