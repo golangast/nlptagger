49237f2e48478fb15981e860ea35f2604d459179
zendrulat <zendrulat@gmail.com>
2024-10-08 11:22:25 +0000

    removed model

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"github.com/golangast/nlptagger/tagger"
+)
+
 func main() {
 
-	// t := tagger.Tagging("Create a database named Inventory with the data structure Product containing 2 string fields and 1 integer field.")
-	// for i := range t.Tokens {
-	// 	fmt.Printf("Tokens: %s\t\t\t Ner: %s\t\t Pos: %s\t\t PhraseTag: %s\t\t  \n", t.Tokens[i], t.NerTag[i], t.PosTag[i], t.PhraseTag[i])
-	// }
+	t := tagger.Tagging("Create a database named Inventory with the data structure Product containing 2 string fields and 1 integer field.")
+	for i := range t.Tokens {
+		fmt.Printf("Tokens: %s\t\t\t Ner: %s\t\t Pos: %s\t\t PhraseTag: %s\t\t  \n", t.Tokens[i], t.NerTag[i], t.PosTag[i], t.PhraseTag[i])
+	}
 	//**VB** create a **NN** webserver **VBN** named **NNP Doggy** with the **NN** handler **NNP Kitty** that **VBZ** has the **NN** data structure **NNP Moose** with **CD** 4 **NN** string fields
 	//t := modelname.Trainer("create a webserver named doggy with the handler kitty that has the data structure moose with 4 string fields")
 	// for i := range t.Tokens {
