046c4b446e38073c44348792a10ef8908931c1a0
zendrulat <zendrulat@gmail.com>
2024-11-14 12:11:04 +0000

    updated main

--- main.go
+++ main.go
@@ -10,29 +10,29 @@ import (
 
 func main() {
 	//actual model data loaded
-	md, err := modeldata.ModelData()
-	if err != nil {
-		fmt.Println("Error loading or training model:", err)
-	}
-	// Example prediction
-	sentence := "generate a webserver with the handler dog with the data structure people"
-	//making prediction
-	predictedPosTags, predictedNerTags, predictedPhraseTags, predictedDRTags := md.PredictTags(sentence)
-	//getting tags
+	// md, err := modeldata.ModelData()
+	// if err != nil {
+	// 	fmt.Println("Error loading or training model:", err)
+	// }
+	// // Example prediction
+	// sentence := "generate a webserver with the handler dog with the data structure people"
+	// //making prediction
+	// predictedPosTags, predictedNerTags, predictedPhraseTags, predictedDRTags := md.PredictTags(sentence)
+	// //getting tags
 
-	predictedTagStruct := tag.Tag{
-		PosTag:          predictedPosTags, // Assign the predicted POS tags to the PosTag field
-		NerTag:          predictedNerTags,
-		PhraseTag:       predictedPhraseTags,
-		DepRelationsTag: predictedDRTags,
-	}
+	// predictedTagStruct := tag.Tag{
+	// 	PosTag:          predictedPosTags, // Assign the predicted POS tags to the PosTag field
+	// 	NerTag:          predictedNerTags,
+	// 	PhraseTag:       predictedPhraseTags,
+	// 	DepRelationsTag: predictedDRTags,
+	// }
 
-	// Print the sentence again for clarity
-	fmt.Println("Sentence:", sentence)
-	// Print the predicted POS tags in a space-separated format
-	fmt.Println("Predicted POS Tag Types:", strings.Join(predictedTagStruct.PosTag, " "))
-	fmt.Println("Predicted NER Tag Types:", strings.Join(predictedTagStruct.NerTag, " "))
-	fmt.Println("Predicted Phrase Tag Types:", strings.Join(predictedTagStruct.PhraseTag, " "))
-	fmt.Println("Predicted Dependency Relation Tag Types:", strings.Join(predictedTagStruct.DepRelationsTag, " "))
+	// // Print the sentence again for clarity
+	// fmt.Println("Sentence:", sentence)
+	// // Print the predicted POS tags in a space-separated format
+	// fmt.Println("Predicted POS Tag Types:", strings.Join(predictedTagStruct.PosTag, " "))
+	// fmt.Println("Predicted NER Tag Types:", strings.Join(predictedTagStruct.NerTag, " "))
+	// fmt.Println("Predicted Phrase Tag Types:", strings.Join(predictedTagStruct.PhraseTag, " "))
+	// fmt.Println("Predicted Dependency Relation Tag Types:", strings.Join(predictedTagStruct.DepRelationsTag, " "))
 
 }
